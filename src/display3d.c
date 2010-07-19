6cc65026f68ed58461d9546d652e55285ed06300
dmarney <dmarney@0b48d15f-8949-407e-9bf2-01e8e60f88e8>
2010-07-19 22:19:51 +0000

    Another attempt to fix bug 2.

--- src/display3d.c
+++ src/display3d.c
@@ -3613,7 +3613,7 @@ static void renderSurroundings(void)
 	pie_SetTexturePage(iV_GetTexture("page-260"));
 
 
-	pie_DrawSkybox(skybox_scale, 0, 0, 1, 1, true);
+	pie_DrawSkybox(skybox_scale, 0, 0, 1, 1, 1);
 	
 	
 	// Load Saved State
@@ -3640,7 +3640,7 @@ static void renderSurroundings(void)
 	{
 		wind = wrapf(wind + graphicsTimeAdjustedIncrement(0.5f), 360.0f);
 	}
-	pie_DrawSkybox(skybox_scale, 0, 0, 1, 1, false);
+	pie_DrawSkybox(skybox_scale, 0, 0, 1, 1, 0);
 	// Load Saved State
 	pie_MatEnd();
 }
