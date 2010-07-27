6ee09d55f3faab41355483c1f79307e1836ad528
stivosaurus <stivosaurus@0b48d15f-8949-407e-9bf2-01e8e60f88e8>
2010-07-27 02:12:48 +0000

    Python script to format WZ Changelogs into bbcode for Trac and forums that support it.  Initial version.

---
+++ build_tools/commitlog2bbcode.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+'''
+script to format Changelog with BBcode tags for posting on webpages.
+Formatted text is sent to standard out
+usage:  log2bbc.py Changelog
+'''
+
+import sys
+import re
+
+
+# revisions - rNNNN
+revPat = re.compile(r'r([0-9]+)')
+revSub = lambda x: '[rev]'+ x.group(1) + '[/rev]'
+
+# tickets - ticket:NNNN
+ticketPat = re.compile( r'ticket:([0-9]+)')
+ticketSub = lambda x: '[ticket]' + x.group(1) + '[/ticket]'
+
+# heading - * Something:
+headPat = re.compile(r'^ \* (.*):$')
+headSub = lambda x: '[b]' + x.group(1) + '[/b]'
+
+# version -  NNNN-NN-NN: some-version
+versionPat = re.compile( r'^\d\d\d\d-\d\d-\d\d:.*$')
+versionSub = lambda x: '[b][u]' + x.group() + '[/u][/b]\n'
+						
+
+if __name__ == '__main__':
+	with open( sys.argv[1], 'r') as f:
+		for line in f.readlines():
+			line = revPat.sub( revSub , line)
+			line = ticketPat.sub( ticketSub, line)
+			line = headPat.sub( headSub, line)
+			line = versionPat.sub( versionSub, line )
+			print line,
+			
+
+
+	
