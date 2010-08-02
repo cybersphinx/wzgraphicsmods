e71977de342f784a3d752443c7694e055bea4ead
dmarney <dmarney@0b48d15f-8949-407e-9bf2-01e8e60f88e8>
2010-08-02 10:23:02 +0000

    Changed filenames as warzone does not like hyphons

--- src/miscimd.c
+++ src/miscimd.c
@@ -79,10 +79,10 @@ static MISC_IMD	miscImds[] =
 	{NULL,"midebr5"},	// debrisImds[4]		MI_DEBRIS4
 
 	{NULL,"fxflecht"},	// met hit - for repair centre MI_FIREWORK
-	{NULL,"light-sphere"},
-	{NULL,"medium-sphere"},
-	{NULL,"heavy-sphere"},
-	{NULL,"super-sphere"},
+	{NULL,"lsphere"},
+	{NULL,"msphere"},
+	{NULL,"hsphere"},
+	{NULL,"ssphere"},
 
 
 
