61e66557d30a71e2325ae2cf8d31c6086e8cc565
le-squeak <le-squeak@0b48d15f-8949-407e-9bf2-01e8e60f88e8>
2010-10-09 03:36:22 +0000

    Change nullptr back to NULL to fix CC erros

--- src/pointtree.cpp
+++ src/pointtree.cpp
@@ -220,8 +220,8 @@ PointTree::ResultVector &PointTree::queryMaybeFilter(Filter &filter, int32_t x, 
 		unsigned i1 = std::lower_bound(points.begin(),      points.end(), Point(ranges[r].a,     NULL)) - points.begin();
 		unsigned i2 = std::lower_bound(points.begin() + i1, points.end(), Point(ranges[r].z + 1, NULL)) - points.begin();
 #else
-		unsigned i1 = std::lower_bound(points.begin(),      points.end(), Point(ranges[r].a,     nullptr)) - points.begin();
-		unsigned i2 = std::lower_bound(points.begin() + i1, points.end(), Point(ranges[r].z + 1, nullptr)) - points.begin();
+		unsigned i1 = std::lower_bound(points.begin(),      points.end(), Point(ranges[r].a,     NULL)) - points.begin();
+		unsigned i2 = std::lower_bound(points.begin() + i1, points.end(), Point(ranges[r].z + 1, NULL)) - points.begin();
 #endif
 		for (unsigned i = current<IsFiltered>(filter.data, i1); i < i2; i = current<IsFiltered>(filter.data, i + 1))
 		{
